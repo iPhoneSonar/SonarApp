226c5ac872f87dd7db3114d758dea5586a780243
KoMa1012 <github.com@kohler-markus.de>
2013-03-05 16:35:56 +0100

    GUI Changes

--- sonar1/processing.h
+++ sonar1/processing.h
@@ -16,25 +16,18 @@
     SInt64 *PKKF;
     SInt32 KKFLen;
     Float64 NetworkLatency;
-    SInt32 KKFZeroDistancePos;
+    SInt32 KKFZeroDistanceSample;
 }
 @property(nonatomic,retain) communicator *com;
 @property(nonatomic) bool isCalibrated;
 @property(nonatomic) SInt64 *PKKF;
 @property(nonatomic) SInt32 KKFLen;
-@property(nonatomic) Float64 NetworkLatency;
-@property(nonatomic) SInt32 KKFZeroDistancePos;
 
 - (void)InitializeArrays;
 - (void)SetSignalDetailsRecord:(SInt32*)ARecord Play:(SInt32*)ASend RecordLen:(SInt32)RecordLen Playlen:(SInt32)PlayLen;
 - (float)CalculateDistanceHeadphone;
-- (void)RingKKF:(SInt64*)AKkf ofRecord:(SInt32*)ARecord AndSend:(SInt32*)ASend RecSamples:(SInt32)NRecordSamples SendSamples:(SInt32)NSendSamples;
 - (SInt32)sendSigGen:(SInt32*)ipBuf :(SInt32&)iBufLen;
-- (void)CalculateLatencyComTimeStamp:(UInt64*)comTimeStamp acTimeStamp:(UInt64*)acTimeStamp nTimeStamps:(SInt32)nTimeStamps;
-
-
-- (void)CalcKKFWithumberOfSamples:(SInt32)Nsamples;
-- (SInt32)MaximumSearchAtStartValue:(UInt32)StartValue WithEndValue:(UInt32)EndValue;
+- (void)CalculateNetworklatencyComTimeStamp:(UInt64*)comTimeStamp acTimeStamp:(UInt64*)acTimeStamp nTimeStamps:(SInt32)nTimeStamps;
 
 @end
 
